37fc5e4aed7ea3708fcc5917a92d645656358f19
charly300 <federico1818@gmail.com>
2014-08-19 18:16:51 -0300

    Se modifica el index para tener enlaces a las paginas de prueba "login" y "registrar"

--- html/index.php
+++ html/index.php
@@ -1,5 +1,20 @@
-<?php
-	
-	echo "INDEX";
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Fulbacho</title>
+	</head>
 
-?>
+
+<body>
+		<div>
+			<a href="login/login.html">Login</a>
+		</div>
+		
+		<div>
+			<a href="registro/registrarse.html">Registrarse</a>
+		</div>
+</body>
+
+
+</html>
