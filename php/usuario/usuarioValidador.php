3c43ce1bb4c06ca9ee24764b8f29905438d13e71
fede90 <rojo.fede@gmail.com>
2014-08-23 15:54:54 -0300

    Se encapsula la funcion empty ya que trae problemas con versiones anteriores de php.

--- php/usuario/usuarioValidador.php
+++ php/usuario/usuarioValidador.php
@@ -19,6 +19,20 @@
 				
 				if(self::estaVacio($user->password))
 					throw new UsuarioValidadorException('La contrase�a no puede estar en blanco');
+				/*
+				 * La función empty permite expesiones a partir de la version 5.5
+				 * */				
+// 				if(empty(trim($user->nombre)))
+// 					throw new UsuarioValidadorException('El nombre no puede estar en blanco');
+// 				if(empty(trim($user->apellido)))
+// 					throw new UsuarioValidadorException('El apellido no puede estar en blanco');
+// 				if(empty(trim($user->mail)))
+// 					throw new UsuarioValidadorException('El email no puede estar en blanco');
+// 				if(!filter_var($user->mail, FILTER_VALIDATE_EMAIL))
+// 					throw new UsuarioValidadorException('El email no tiene un formato válido');
+// 				if(empty(trim($user->password)))
+// 					throw new UsuarioValidadorException('La contraseña no puede estar en blanco');
+				
 			}
 			
 			private function estaVacio($string){
