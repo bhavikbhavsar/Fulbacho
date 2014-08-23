a0eb295d165deb03a427e3296da1561f47957cbe
fede90 <rojo.fede@gmail.com>
2014-08-22 22:15:39 -0300

    se encapsula la función empty ya que suele traer problemas

--- php/usuario/usuarioValidador.php
+++ php/usuario/usuarioValidador.php
@@ -5,20 +5,25 @@
 			
 			public static function validar(Usuario $user){
 					
-				if(empty(trim($user->nombre)))
+				if(self::estaVacio($user->nombre))
 					throw new UsuarioValidadorException('El nombre no puede estar en blanco');
 				
-				if(empty(trim($user->apellido)))
+				if(self::estaVacio($user->apellido))
 					throw new UsuarioValidadorException('El apellido no puede estar en blanco');
 				
-				if(empty(trim($user->mail)))
+				if(self::estaVacio($user->mail))
 					throw new UsuarioValidadorException('El email no puede estar en blanco');
 				
 				if(!filter_var($user->mail, FILTER_VALIDATE_EMAIL))
-					throw new UsuarioValidadorException('El email no tiene un formato válido');
+					throw new UsuarioValidadorException('El email no tiene un formato vï¿½lido');
 				
-				if(empty(trim($user->password)))
-					throw new UsuarioValidadorException('La contraseña no puede estar en blanco');
+				if(self::estaVacio($user->password))
+					throw new UsuarioValidadorException('La contraseï¿½a no puede estar en blanco');
+			}
+			
+			private function estaVacio($string){
+				$stringLimpio = trim($string);
+				return empty($stringLimpio);
 			}
 		}
 	
